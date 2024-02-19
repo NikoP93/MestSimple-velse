7c8a7fde8d6e24ec9ad72e8e64e2d8a927926c94
NikoP93 <nipe0001@stud.kea.dk>
2024-02-19 13:44:19 +0100

    Changed text

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Create Attraction");
+        System.out.println("Create Tourist Attraction");
     }
 }
